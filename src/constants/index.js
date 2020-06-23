ff3c836c1cadef215abaa34784d5ed40ba21456a
Barry Rollan <barryrollan@macbook-pro.lan>
2020-06-22 22:09:49 -0400

    moved token out of .env file because is was causing issues in production, will research how to store envirnmental variables on netlify

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,7 +2,8 @@ const sanityClient = require('@sanity/client');
 export const Client = sanityClient({
   projectId: 'ogg4t6rs',
   dataset: 'production',
-  token: process.env.REACT_APP_API_KEY,
+  token:
+    'sktPD2r791blYmo8n26ZCurNfamiwCJ2KfgbdmPsIYPFGywjAK4roSijSwqTsH83LYiPvFIfDmOH1JL5jtzjGdpADZoEVIaKxzv8vJyD4Wj8lX04qNqzLEbVDN3uLAoEFRNWgLJga6t6LCSV6JGMOiiXG9MtjWVXdyxgHmQfWik5siHH65dt',
   useCdn: false, // `false` if you want to ensure fresh data
   ignoreBrowserTokenWarning: true,
 });
